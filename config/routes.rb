219d555b245940631d232068bcd4e9b416762079
Mahima Devanahalli <mahima.dev1999@gmail.com>
2021-03-08 12:18:56 -0500

    added sign up functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :mentors
   resources :users
+  root 'users#new'
+  get  '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
