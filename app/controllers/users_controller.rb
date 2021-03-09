f34f0c4f664ae537fcf700913b60e53f7213c814
Mahima Devanahalli <mahima.dev1999@gmail.com>
2021-03-08 19:43:49 -0500

    sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
-    
-    def show
-        @user = User.find(params[:id])
-      end
-    
-      def new
-        @user = User.new
-      end
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    respond_to do |format| format.html{redirect_to '/', notice: "sucess"} end
+  end
+
+  def user_params
+    params.require(:user).permit(:first, :last, :email, :major)
+  end
 end
