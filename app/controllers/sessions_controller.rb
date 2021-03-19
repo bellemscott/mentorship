49c9d0944e70678d36b60116c63f49268e0a1580
belle scott <bscott@brandeis.edu>
2021-03-19 11:22:12 -0400

    remember me token and cookie protection

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,12 @@ class SessionsController < ApplicationController
     def create
       user = User.find_by(email: params[:session][:email].downcase)
       if user && user.authenticate(params[:session][:password])
+        reset_session
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         log_in user
-        redirect_back_or user
+        redirect_to root_url
+        # log_in user
+        # redirect_back_or user
       else
         flash.now[:danger] = 'Invalid email/password combination'
         render 'new'
@@ -14,7 +18,7 @@ class SessionsController < ApplicationController
     end
   
     def destroy
-      log_out
+      log_out if logged_in?
       redirect_to root_url
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
     has_secure_password
     before_save { self.email = email.downcase }
     validates :firstname, presence: true
@@ -6,4 +7,37 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
     validates :password, presence: true, length: { minimum: 6 }
+
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                      BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+      end
+
+
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+    
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+        remember_digest
+    end
+
+      # Returns a session token to prevent session hijacking.
+    # We reuse the remember digest for convenience.
+    def session_token
+        remember_digest || remember
+    end
+
+    def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    # Forgets a user.
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
 end
