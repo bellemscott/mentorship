e9bf43cfbfcd63541457daca77b69098d50668b4
Avaharnick <avaharnick@brandeis.edu>
2021-03-26 15:50:33 -0400

    matches

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,71 +1,62 @@
 class MatchesController < ApplicationController
-    before_action :set_enrollment, only: %i[ show edit update destroy ]
-  
-    def index
-      @matches = Match.all
-      @user = current_user
-    end
-  
-    def show
-    end
-  
-    def new
-      @match = Match.new
-    end
-  
-    def edit
-    end
-  
-    def create
-        @match = Match.new(match_params)
-        matches = Match.all
-        repititions = 0
-        matches.each do |match|
-            if
-            end
-        end
-        if repititions == 0
-            if @enrollment.save
-                flash[:sucess] = "succesfully enrolled"
-                redirect_to enroll_path
-            else
-                render 'new'
-            end
-        else
-            flash[:alert] = "already enrolled in course"        #wont let user enroll in same course twice
-        end
+    before_action :set_match, only: %i[ show edit update destroy ]
 
-    end
-  
-    def update
-      respond_to do |format|
-        if @enrollment.update(enrollment_params)
-          format.html { redirect_to @enrollment, notice: "Enrollment was successfully updated." }
-          format.json { render :show, status: :ok, location: @enrollment }
-        else
-          format.html { render :edit, status: :unprocessable_entity }
-          format.json { render json: @enrollment.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-  
-    def destroy
-      @enrollment.destroy
-      respond_to do |format|
-        format.html { redirect_to enrollments_url, notice: "Enrollment was successfully destroyed." }
-        format.json { head :no_content }
-      end
-    end
-  
-    private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_enrollment
-        @enrollment = Enrollment.find(params[:id])
-      end
-  
-      # Only allow a list of trusted parameters through.
-      def enrollment_params
-        params.require(:enrollment).permit(:user_id, :course_id, :coursename, :coursedescription)
-      end
+  # GET /mentors or /mentors.json
+  def index
+    create()
+    @mentors = Mentor.all
+    @mentees=Mentee.all
+    @users=User.all
+    @matches_for_user = @matches_arr
+    #@users = User.all
   end
+
+  # GET /mentors/1 or /mentors/1.json
+  def show
+    
+  end
+
+  # GET /mentors/new
+  def new
+    @match = Match.new
+  end
+
+  # GET /mentors/1/edit
+  def edit
+  end
+
+  # POST /mentors or /mentors.json
+  def create
+    # @user = @users.last
+    @mentors = Mentor.all
+    @mentees= Mentee.all 
+    puts @mentees.length()
+
+   # @major_to_curent_position #implement map
+    @matches_arr=[] 
+    @mentors.each do |mentor|
+        @mentess.each do |mentee|
+            if mentee.user_id != mentor.user_id 
+                if mentor.length_of_mentorship==mentee.length_of_mentorship
+                    @newMatch= Match.new(current_user.id,mentor.id,@entee.id)
+                    @matches_arr.push(@newMatch)
+                end
+          
+            end
+        end
+    end
+end
+    
+
+
+
+  def set_match
+    
+  end
+
+  # Only allow a list of trusted parameters through.
+  def mentor_params
+
+  end
+
 end
