9b93cfa1741ff5340326881b692dd044e4672ce4
Mahima Devanahalli <mahima.dev1999@gmail.com>
2021-04-05 16:13:11 -0400

    mentor_test

--- test/models/mentor_test.rb
+++ test/models/mentor_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class MentorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @mentor = Mentor.new(area_of_expertise: "Software Engineering", preferred_method_of_contact: "Text", school: "Brandeis", 
+      current_position: "Manager", location: "Boston", length_of_mentorship: "1", user_id: "1")
+    @mentor_nullfield = Mentor.new(area_of_expertise: "", preferred_method_of_contact: "Text", school: "Brandeis", 
+        current_position: "Manager", location: "Boston", length_of_mentorship: "1", user_id: "1")
+    @mentor_emptyform = Mentor.new(area_of_expertise: "", preferred_method_of_contact: "", school: "", 
+          current_position: "", location: "", length_of_mentorship: "", user_id: "")
+  end
+
+  test "should be valid" do
+    assert @mentor.valid?
+    assert @mentor_nullfield.valid?
+    assert @mentor_emptyform.valid?
+  end
+
 end
