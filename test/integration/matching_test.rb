fe0a278924ef83b163f1c6808eb70e6add6c66f2
Mahima Devanahalli <mahima.dev1999@gmail.com>
2021-04-07 17:12:56 -0400

    integration test

--- test/integration/matching_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class MatchingTest < ActiveSupport::TestCase
-    # create 2 users, one mentee one mentor, make sure they have fields they can match on (length of mentorship)
-    # call match create/new action in controller
-    # assert that the match record contains the correct information
-end
