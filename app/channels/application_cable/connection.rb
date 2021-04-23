5fec70ee9be34fe736d320575199123cf4858612
belle scott <bscott@brandeis.edu>
2021-04-23 14:38:37 -0400

    testing master

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,33 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    def deliver
+      channel.broadcast_to recipient, format
+    end
+
+    private
+
+    def format
+      if (method = options[:format])
+        notification.send(method)
+      else
+        notification.params
+      end
+    end
+
+    def channel
+      @channel ||= begin
+        value = options[:channel]
+        case value
+        when String
+          value.constantize
+        when Symbol
+          notification.send(value)
+        when Class
+          value
+        else
+          Noticed::NotificationChannel
+        end
+      end
+    end
   end
 end
